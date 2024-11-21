a3cab59cfdfddf19e38fa973bb03dcd5b141f5bd
Athenkosi Magada <magadaathenkosi@gmail.com>
2024-11-21 14:36:33 +0200

    t: add Portfolio section and Footer components
    - Introduced a Portfolio section to showcase highlight projects.
    - Added a Footer component with navigational links and copyright
      details.
      - Improved site structure for enhanced usability and professional
        presentation.

--- src/app/pages/home/portfolio/projects-list/projects-list.component.ts
+++ src/app/pages/home/portfolio/projects-list/projects-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ProjectComponent } from "../../../../components/project/project.component";
+import { LoadButtonComponent } from "../../../../components/load-button/load-button.component";
 
 @Component({
   selector: 'app-projects-list',
   standalone: true,
-  imports: [ProjectComponent],
+  imports: [ProjectComponent, LoadButtonComponent],
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.css']
 })
