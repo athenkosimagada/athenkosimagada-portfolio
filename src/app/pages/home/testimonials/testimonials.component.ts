5fca2f582c50285d557c98f60504b27f3193cc92
Athenkosi Magada <magadaathenkosi@gmail.com>
2024-11-22 19:57:21 +0200

    timonials): remove unused imports
    - Cleaned up unnecessary imports from the testimonials component
    - Improved code readability and maintainability

--- src/app/pages/home/testimonials/testimonials.component.ts
+++ src/app/pages/home/testimonials/testimonials.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { SectionParagraphComponent } from "../../../components/section-paragraph/section-paragraph.component";
 import { TestimonialsListComponent } from "./testimonials-list/testimonials-list.component";
 import { SectionHeaderComponent } from "../../../components/section-header/section-header.component";
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [SectionParagraphComponent, TestimonialsListComponent, SectionHeaderComponent],
+  imports: [TestimonialsListComponent, SectionHeaderComponent],
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.css']
 })
