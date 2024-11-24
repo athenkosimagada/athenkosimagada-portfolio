f7be0431251c37f3d8c7695f60d010cdb6ec386c
Athenkosi Magada <magadaathenkosi@gmail.com>
2024-11-25 00:23:59 +0200

    chore: add vercel speed-insights

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
  
+injectSpeedInsights();
 inject();
 
 bootstrapApplication(AppComponent, appConfig)
