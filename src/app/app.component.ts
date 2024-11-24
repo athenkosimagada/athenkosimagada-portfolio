8f0b2c297f2f6dcb67b6d903e1351d3301bde90d
Athenkosi Magada <magadaathenkosi@gmail.com>
2024-11-25 00:14:38 +0200

    chore: add vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from "./components/footer/footer.component";
 import { register } from 'swiper/element/bundle';
 import { ContactSectionComponent } from './components/contact-section/contact-section.component';
+import { Analytics } from "@vercel/analytics/react";
+
 register();
 @Component({
   selector: 'app-root',
