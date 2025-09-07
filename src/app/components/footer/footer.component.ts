15e83a9cd080c01294143e586e0658d7345a6f97
Athenkosi Magada <magadaathenkosi@gmail.com>
2025-09-07 05:28:52 +0200

    feat: add analytics and email services for event tracking and email functionality

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { LogoComponent } from "../logo/logo.component";
-import { NavLinksComponent } from "../nav-links/nav-links.component";
-import { SocialLinksComponent } from "../social-links/social-links.component";
-import { CopyrightComponent } from "./copyright/copyright.component";
+import { Component, OnInit } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+import { NavLinksComponent } from '../nav-links/nav-links.component';
+import { SocialLinksComponent } from '../social-links/social-links.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { AnalyticsService } from '../../services';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LogoComponent, NavLinksComponent, SocialLinksComponent, CopyrightComponent],
+  imports: [
+    LogoComponent,
+    NavLinksComponent,
+    SocialLinksComponent,
+    CopyrightComponent,
+  ],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(private analyticsService: AnalyticsService) {}
 
+  ngOnInit() {
+    this.analyticsService.trackEvent('page_view', 'footer', 'engagement');
+  }
 }
---
+++ src/app/services/analytics.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+declare var gtag: any;
+
+@Injectable({ providedIn: 'root' })
+export class AnalyticsService {
+  trackEvent(eventName: string, eventDetails: string, eventCategory: string) {
+    gtag('event', eventName, {
+      event_category: eventCategory,
+      event_label: eventName,
+      value: eventDetails,
+    });
+  }
+}
